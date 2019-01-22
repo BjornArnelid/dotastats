d0ace07183b7e59cbd4f57be9721566ed9f03892
Björn Arnelid <bjorn.arnelid@gmail.com>
2019-01-22 11:18:59 +0100

    Improving counters view

--- statscontroller.py
+++ statscontroller.py
@@ -1,45 +1,29 @@
 import requests
 import json
 from perspectives import QuantityPerspective, WinratePerspective, DiffPerspective
-from hero import Hero
+from hero import Hero, get_hero_from_id
 
 
 class StatsController(object):
-    def __init__(self, player_id, sample, mode, allies):
-        if sample:
-            attributes = '?limit=%s&' % sample
+    def __init__(self, player_id, args):
+        if args.get('sample'):
+            attributes = '?limit=%s&' % args.get('sample')
         else:
             attributes = '?'
-        if mode == 'turbo':
+        if args.get('mode') == 'turbo':
             attributes += 'significant=0&game_mode=23'
-        elif mode == 'ranked':
+        elif args.get('mode') == 'ranked':
             attributes += 'lobby_type=7'
-        elif mode == 'unranked':
+        elif args.get('mode') == 'unranked':
             attributes += 'lobby_type=0'
-        if allies:
-            attributes += '&included_account_id=%s' % allies
+        if args.get('allies'):
+            attributes += '&included_account_id=%s' % args.get('allies')
         self.request_string = 'https://api.opendota.com/api/players/%s/heroes%s' % (player_id, attributes)
 
     def get_suggestions(self, sort_order):
         response = requests.get(self.request_string)
         input_data = json.loads(response.text)
-        return self._parse_suggestions(input_data, sort_order)
-
-    def _parse_suggestions(self, input_data, sort_order):
-        games = 0
-        wins = 0
-        picks = []
-        bans = []
-        for hero in input_data:
-            games += hero['games']
-            h = Hero(hero)
-            if hero['win'] > 0:
-                wins += hero['win']
-                picks.append(h)
-            if hero['against_games'] > 0:
-                bans.append(h)
-        win_percentage = (wins / int(games)) * 100
-        picks = [h for h in picks if h.winrate > win_percentage]
+        result = _parse_input_data(input_data)
         if sort_order == 'winrate':
             perspective = WinratePerspective()
         elif sort_order == 'quantity':
@@ -47,12 +31,42 @@ class StatsController(object):
         else:
             sort_order = 'diff'
             perspective = DiffPerspective()
-        picks = sorted(picks, key=perspective.sort_picks, reverse=True)
-        bans = perspective.filter_bans(bans, win_percentage)
-        bans = sorted(bans, key=perspective.sort_bans, reverse=True)
-        return {'avg_win': win_percentage, 'sample': games, 'picks': picks, 'bans': bans}
+        picks = [h for h in result['picks'] if h.winrate > result['avg_win']]
+        result['picks'] = sorted(picks, key=perspective.sort_picks, reverse=True)
+        bans = perspective.filter_bans(result['bans'], result['avg_win'])
+        result['bans'] = sorted(bans, key=perspective.sort_bans, reverse=True)
+        return result
 
     def get_counter(self, hero_id, sort_order):
         response = requests.get(self.request_string + '&against_hero_id=%s' % hero_id)
         input_data = json.loads(response.text)
-        return self._parse_suggestions(input_data, sort_order)
+        result = _parse_input_data(input_data)
+        if sort_order == 'winrate':
+            perspective = WinratePerspective()
+        elif sort_order == 'quantity':
+            perspective = QuantityPerspective()
+        else:
+            sort_order = 'diff'
+            perspective = DiffPerspective()
+        picks = [h for h in result['picks'] if h.winrate > result['avg_win']]
+        result['picks'] = sorted(picks, key=perspective.sort_picks, reverse=True)
+        del result['bans']
+        result['counter_hero'] = get_hero_from_id(hero_id)['localized_name']
+        return result
+
+
+def _parse_input_data(input_data):
+    games = 0
+    wins = 0
+    picks = []
+    bans = []
+    for hero in input_data:
+        games += hero['games']
+        h = Hero(hero)
+        if hero['win'] > 0:
+            wins += hero['win']
+            picks.append(h)
+        if hero['against_games'] > 0:
+            bans.append(h)
+    win_percentage = (wins / int(games)) * 100
+    return {'avg_win': win_percentage, 'sample': games, 'picks': picks, 'bans': bans}
--- wsgi.py
+++ wsgi.py
@@ -28,38 +28,34 @@ def redirect_picks():
 
 @application.route('/picks/<player_id>')
 def get_picks(player_id):
-    sample = request.args.get('sample')
-    mode = request.args.get('mode')
-    sort_order = request.args.get('sortOrder')
-    allies = request.args.get('allies')
     try:
-        controller = StatsController(player_id, sample, mode, allies)
-        result = controller.get_suggestions(sort_order)
-        result['mode'] = mode
+        controller = StatsController(player_id, request.args)
+        result = controller.get_suggestions(request.args.get('sortOrder'))
         if request.accept_mimetypes.accept_html:
-            return flask.render_template('picks.html', result=result, id=player_id)
+            return flask.render_template('picks.html', result=result, id=player_id, mode=request.args['mode'], query=request.query_string.decode('UTF-8'))
         else:
             flask.abort(415)
     except TypeError:
         flask.abort(400)
 
 
-@application.route('/picks/<player_id>/counters/<hero_id>')
+@application.route('/picks/<player_id>/counterpicks/<hero_id>')
 def get_counters(player_id, hero_id):
     sample = request.args.get('sample')
     mode = request.args.get('mode')
     sort_order = request.args.get('sortOrder')
     allies = request.args.get('allies')
     try:
-        controller = StatsController(player_id, sample, mode, allies)
+        controller = StatsController(player_id, request.args)
         result = controller.get_counter(hero_id, sort_order)
         result['mode'] = mode
         if request.accept_mimetypes.accept_html:
-            return flask.render_template('picks.html', result=result, id=player_id)
+            return flask.render_template('counters.html', result=result, id=player_id, mode=request.args['mode'], query=request.query_string.decode('UTF-8'))
         else:
             flask.abort(415)
     except TypeError:
         flask.abort(400)
+
 
 if __name__ == '__main__':
         application.run()
