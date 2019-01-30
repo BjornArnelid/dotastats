8372af751c7345a01027215558384bd6d8c34202
Björn Arnelid <bjorn.arnelid@gmail.com>
2019-01-30 09:40:27 +0100

    Renamed API methods so adresses makes more sense.

--- wsgi.py
+++ wsgi.py
@@ -13,9 +13,10 @@ def landing_page():
     return flask.render_template('index.html', request=request.args)
 
 
-@application.route('/picks', methods=['GET'])
+@application.route('/redirect', methods=['GET'])
 def redirect_picks():
-    return flask.redirect(url_for('get_picks',
+    # Inform user if id is wrong
+    return flask.redirect(url_for('get_suggestions',
                                   player_id=request.values['id'],
                                   sample=request.values['sample'],
                                   allies=request.values['allies'],
@@ -23,30 +24,36 @@ def redirect_picks():
                                   sortOrder=request.values['sort']))
  
 
-@application.route('/picks/<player_id>')
-def get_picks(player_id):
+@application.route('/<player_id>/suggestions')
+def get_suggestions(player_id):
     try:
         controller = StatsController(player_id, request.args)
         result = controller.get_suggestions(request.args.get('sortOrder'))
-    except TypeError:
-        flask.abort(400)
+    except (TypeError, ZeroDivisionError):
+        flask.abort(422)
     if request.accept_mimetypes.accept_html:
-        return flask.render_template('picks.html', result=result, id=player_id, mode=request.args.get('mode'), query=request.query_string.decode('UTF-8'))
+        return flask.render_template('suggestions.html', result=result, id=player_id, mode=request.args.get('mode'), query=request.query_string.decode('UTF-8'))
     else:
         flask.abort(415)
 
 
-@application.route('/picks/<player_id>/counterpicks/<hero_id>')
+@application.route('/suggestions')
+def empty_suggestions():
+    return flask.redirect(url_for('landing_page'))
+
+
+@application.route('/<player_id>/counters/<hero_id>')
 def get_counters(player_id, hero_id):
     try:
         controller = StatsController(player_id, request.args)
         result = controller.get_counter(hero_id, request.args.get('sortOrder'))
-    except TypeError:
-        flask.abort(400)
+    except (TypeError, ZeroDivisionError):
+        flask.abort(422)
     if request.accept_mimetypes.accept_html:
         return flask.render_template('counters.html', result=result, id=player_id, mode=request.args.get('mode'), query=request.query_string.decode('UTF-8'))
     else:
         flask.abort(415)
+
 
 if __name__ == '__main__':
         application.run()
